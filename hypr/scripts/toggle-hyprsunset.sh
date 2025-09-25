5a6681dd007523d41008211e8c8899086b9d7f42
ryuzinoh <140276215+RyuZinOh@users.noreply.github.com>
2025-09-26 01:10:48 +0545

    some stuff addition/and libnotify updates/mako, add ttyper config

--- hypr/scripts/toggle-hyprsunset.sh
+++ hypr/scripts/toggle-hyprsunset.sh
@@ -2,9 +2,9 @@
 
 if pgrep -x "hyprsunset" > /dev/null
 then
-  pkill hyprsunset
-  hyprctl notify -1 3000 "rgb(003366)" "fontsize:20 Hyprsunset OFF"
+    pkill hyprsunset
+    notify-send "󰃚 Hyprsunset OFF"
 else
-  hyprsunset --temperature 4000 --gamma 80 &
-  hyprctl notify -1 3000 "rgb(003366)" "fontsize:20 Hyprsunset ON"
+    hyprsunset --temperature 4000 --gamma 80 &
+    notify-send "󰖚 Hyprsunset ON"
 fi
--- hypr/scripts/toggle-recordwf.sh
+++ hypr/scripts/toggle-recordwf.sh
@@ -3,22 +3,18 @@
 PIDFILE="/tmp/wl-screenrec.pid"
 OUTPUT="$HOME/Videos/recording_$(date +%s).mp4"
 
-notify() {
-    hyprctl notify 1 3000 "rgb(ffffff)" "$1"
-}
-
 if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")" 2>/dev/null; then
     kill -INT "$(cat "$PIDFILE")"
     rm -f "$PIDFILE"
-    notify "Recording Stopped"
+    notify-send "  Recording Stopped"
 else
     wl-screenrec --filename "$OUTPUT" &
     PID=$!
     sleep 0.5
     if kill -0 "$PID" 2>/dev/null; then
         echo "$PID" > "$PIDFILE"
-        notify "Recording Started"
+        notify-send " Recording Started"
     else
-        notify "Failed to start recording"
+        notify-send "  Failed to start recording"
     fi
 fi
