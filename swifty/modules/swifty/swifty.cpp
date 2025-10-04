7804ddbec3980b49b8738b4914b531be312c7e63
ryuzinoh <140276215+RyuZinOh@users.noreply.github.com>
2025-10-04 16:20:16 +0545

    swifty/ kinematics correction for flow state, and thumbnails auto deletion when they are orphaned

--- swifty/modules/swifty/swifty.cpp
+++ swifty/modules/swifty/swifty.cpp
@@ -12,56 +12,90 @@
 Swifty::Swifty(QWidget *parent) : QWidget(parent) {
     setFixedSize(1575, 200);
     setStyleSheet("background-color: black; border-radius: 8px;");
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
+
     scrollArea = new KineticScrollArea(this);
-    scrollArea->setGeometry(0, 0, width(), height());
+    scrollArea->setGeometry(rect());
     scrollArea->setStyleSheet("background: black; border: none;");
+
     containerWidget = new QWidget();
     containerWidget->setStyleSheet("background: black;");
     hLayout = new QHBoxLayout(containerWidget);
     hLayout->setSpacing(8);
     hLayout->setContentsMargins(5, 1, 5, 1);
+
     scrollArea->setWidget(containerWidget);
+
+    cleanupCache();
     loadWallpapers();
     scrollArea->setLabels(labels);
 }
 
 void Swifty::showEvent(QShowEvent *) {
-    QScreen *screen = QGuiApplication::primaryScreen();
-    if (!screen) return;
-    move((screen->geometry().width() - width()) / 2,
-         screen->geometry().height() - height() - 5);
+    if (QScreen *screen = QGuiApplication::primaryScreen()) {
+        QRect geom = screen->geometry();
+        move((geom.width() - width()) / 2,
+             geom.height() - height() - 5);
+    }
+}
+
+QString Swifty::swiftyCachePath() const {
+    QDir dir(QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/swifty");
+    dir.mkpath(".");
+    return dir.absolutePath();
+}
+
+void Swifty::cleanupCache() {
+    QDir cacheDir(swiftyCachePath());
+    QStringList cacheFiles = cacheDir.entryList({"*.jpg"}, QDir::Files);
+
+    QDir picturesDir(QStandardPaths::writableLocation(QStandardPaths::PicturesLocation));
+    QStringList filters = {"*.jpg","*.jpeg","*.png","*.gif"};
+    QFileInfoList files = picturesDir.entryInfoList(filters, QDir::Files);
+
+    QSet<QString> validHashes;
+    for (const QFileInfo &file : files)
+        validHashes.insert(QString(QCryptographicHash::hash(file.absoluteFilePath().toUtf8(), QCryptographicHash::Sha1).toHex()));
+
+    for (const QString &thumbFile : cacheFiles) {
+        QString hashName = QFileInfo(thumbFile).completeBaseName();
+        if (!validHashes.contains(hashName))
+            QFile::remove(cacheDir.filePath(thumbFile));
+    }
 }
 
 void Swifty::loadWallpapers() {
-    QDir dir(QStandardPaths::writableLocation(QStandardPaths::PicturesLocation));
+    QDir picturesDir(QStandardPaths::writableLocation(QStandardPaths::PicturesLocation));
     QStringList filters = {"*.jpg","*.jpeg","*.png","*.gif"};
-    QFileInfoList files = dir.entryInfoList(filters, QDir::Files);
-    QDir cacheDir(QStandardPaths::writableLocation(QStandardPaths::CacheLocation));
-    cacheDir.mkpath("swifty");
-    QString cachePath = cacheDir.filePath("swifty");
+    QFileInfoList files = picturesDir.entryInfoList(filters, QDir::Files);
+
+    QString cachePath = swiftyCachePath();
 
     for (const QFileInfo &file : files) {
-        QString imagePath = file.absoluteFilePath();
-        QString wallpaperName = file.completeBaseName();
-        QByteArray hash = QCryptographicHash::hash(imagePath.toUtf8(), QCryptographicHash::Sha1).toHex();
+        QString path = file.absoluteFilePath();
+        QByteArray hash = QCryptographicHash::hash(path.toUtf8(), QCryptographicHash::Sha1).toHex();
         QString thumbPath = cachePath + "/" + hash + ".jpg";
+
         QPixmap thumbnail;
-        if (QFile::exists(thumbPath))
-            thumbnail.load(thumbPath);
-        else {
-            QImage img(imagePath);
+        if (!QFile::exists(thumbPath)) {
+            QImage img(path);
             if (img.isNull()) continue;
-            int h=180,w=img.width()*h/img.height();
-            QImage thumb = img.scaled(w,h,Qt::KeepAspectRatio,Qt::SmoothTransformation);
-            thumb.save(thumbPath);
-            thumbnail = QPixmap::fromImage(thumb);
+            int h = 180;
+            int w = img.width() * h / img.height();
+            img = img.scaled(w, h, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+            img.save(thumbPath);
+            thumbnail = QPixmap::fromImage(img);
+        } else {
+            thumbnail.load(thumbPath);
         }
+
         if (thumbnail.isNull()) continue;
-        ClickableLabel *label = new ClickableLabel(imagePath);
+
+        ClickableLabel *label = new ClickableLabel(path);
         label->setPixmap(thumbnail);
         label->setFixedSize(thumbnail.size());
-        connect(label,&ClickableLabel::clicked,this,&Swifty::applyWallpaper);
+        connect(label, &ClickableLabel::clicked, this, &Swifty::applyWallpaper);
+
         hLayout->addWidget(label);
         labels.append(label);
     }
@@ -70,22 +104,15 @@ void Swifty::loadWallpapers() {
 void Swifty::applyWallpaper(const QString &path) {
     QString wallpaperName = QFileInfo(path).completeBaseName();
 
-    QProcess::startDetached("notify-send", { 
-        wallpaperName + " Applied!" 
-    });
-  
-    QProcess::startDetached("swww", {"img", path,
-                                     "--transition-type", "wipe",
-                                     "--transition-duration", "1",
-                                     "--transition-fps", "60"});
+    QProcess::startDetached("notify-send", {wallpaperName + " Applied!"});
+    QProcess::startDetached("swww", {"img", path, "--transition-type", "wipe", "--transition-duration", "1", "--transition-fps", "60"});
 
     QString hyprlockDir = "/home/safal726/.cache/hyprlock-safal";
+    QDir dir(hyprlockDir);
+    if (!dir.exists()) dir.mkpath(".");
+
     QString finalPath = hyprlockDir + "/bg.jpg";
 
-    QDir().mkpath(hyprlockDir);
-
     QImage img(path);
-    if (!img.isNull()) {
-        img.save(finalPath, "JPG");  
-    }
+    if (!img.isNull()) img.save(finalPath, "JPG");
 }
--- swifty/modules/swifty/swifty.h
+++ swifty/modules/swifty/swifty.h
@@ -8,15 +8,23 @@
 class Swifty : public QWidget {
     Q_OBJECT
 public:
-    Swifty(QWidget *parent = nullptr);
+    explicit Swifty(QWidget *parent = nullptr);
+
 protected:
-    void showEvent(QShowEvent *) override;
+    void showEvent(QShowEvent *event) override;
+
 private:
     KineticScrollArea *scrollArea;
     QWidget *containerWidget;
     QHBoxLayout *hLayout;
     QList<ClickableLabel *> labels;
+
     void loadWallpapers();
+    void positionRibbon();
+    void cleanupCache();
+    QString swiftyCachePath() const;
+    QString hyprlockPath() const;
+
 private slots:
     void applyWallpaper(const QString &path);
 };
