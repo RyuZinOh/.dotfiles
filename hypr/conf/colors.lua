e8f09ed6cee42c69da7ab730c2920357728c3d10
RyuzinOh <140276215+RyuZinOh@users.noreply.github.com>
2026-05-11 16:40:50 +0545

    publish lua template

---
+++ hypr/conf/colors.lua
@@ -0,0 +1,5 @@
+return {
+	active_border_1 = "rgba(faabffee)",
+	active_border_2 = "rgba(ffb2baee)",
+	inactive_border = "rgba(4f434eaa)",
+}
---
+++ matugen/templates/colors.lua
@@ -0,0 +1,5 @@
+return {
+	active_border_1 = "rgba({{colors.primary.default.hex_stripped}}ee)",
+	active_border_2 = "rgba({{colors.tertiary.default.hex_stripped}}ee)",
+	inactive_border = "rgba({{colors.outline_variant.default.hex_stripped}}aa)",
+}
